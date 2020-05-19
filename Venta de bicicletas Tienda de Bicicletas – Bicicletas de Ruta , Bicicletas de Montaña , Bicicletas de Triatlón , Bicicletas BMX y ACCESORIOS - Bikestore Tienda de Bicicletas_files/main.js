0da4f1054db651ef3975403ad9d8361e3d2e1f89
vbart9379 <64481841+vbart9379@users.noreply.github.com>
2020-05-19 11:20:52 +0300

    1

--- Venta de bicicletas Tienda de Bicicletas – Bicicletas de Ruta , Bicicletas de Montaña , Bicicletas de Triatlón , Bicicletas BMX y ACCESORIOS - Bikestore Tienda de Bicicletas_files/main.js
+++
@@ -1,9 +0,0 @@
-var alturaHeader=jQuery("header").height();
-jQuery(".page-container").css("margin-top",alturaHeader);
-
-jQuery("#menu3 .parentMenu a").prepend("<i class='i-bicicletas icon'></i>");
-jQuery("#menu365 .parentMenu a").prepend("<i class='i-equipo icon'></i>");
-jQuery("#menu309 .parentMenu a").prepend("<i class='i-componentes icon'></i>");
-jQuery("#menu6 .parentMenu a").prepend("<i class='i-sales icon'></i>");
-jQuery("#menu310 .parentMenu a").prepend("<i class='i-ropa icon'></i>");
-jQuery("#menu4 .parentMenu a").prepend("<i class='i-marcas icon'></i>");
